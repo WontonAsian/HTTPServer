3cd768d58d1d393f4320c7ef9372aab628ce2091
Kevin Nguyen <97388692+WontonAsian@users.noreply.github.com>
2023-04-24 22:10:01 -0700

    http server

--- HttpServer/src/MyHttpServer.java
+++ HttpServer/src/MyHttpServer.java
@@ -1,33 +1,30 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MyHttpServer {
 
-  private static final String BASE_PATH = "/Users/hoangnguyen/Desktop/INFO 314/HTTP/HttpServer";
+    private static final String BASE_PATH = "/Users/hoangnguyen/Desktop/INFO 314/HTTPServer/HttpServer";
 
-  private static final Map<String, String> MIME_TYPES = new HashMap<String, String>() {{
-    put("txt", "text/plain");
-    put("html", "text/html");
-  }};
+    private static final Map<String, String> MIME_TYPES = new HashMap<>() {{
+        put("txt", "text/plain");
+        put("html", "text/html");
+        put("json", "text/json");
+    }};
 
-  public static void main(String[] args) throws Exception {
-    ServerSocket serverSocket = new ServerSocket(8000);
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8000);
 
-    while (true) {
-      Socket socket = serverSocket.accept();
-      HttpRequestHandler handler = new HttpRequestHandler(socket);
-      handler.start();
+        while (true) {
+            Socket socket = serverSocket.accept();
+            HttpRequestHandler handler = new HttpRequestHandler(socket);
+            handler.start();
+        }
     }
-  }
 
   static class HttpRequestHandler extends Thread {
     private final Socket socket;
@@ -38,40 +35,95 @@ public class MyHttpServer {
 
     public void run() {
       try {
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-        String requestLine = in.readLine();
-        String[] requestLineParts = requestLine.split(" ");
-        String requestMethod = requestLineParts[0];
-        String requestPath = requestLineParts[1];
-        String httpVersion = requestLineParts[2];
-
-        if (!requestMethod.equals("GET") && !requestMethod.equals("POST") && !requestMethod.equals("PUT") && !requestMethod.equals("DELETE")) {
-          sendErrorResponse(out, 405);
-          return;
-        }
-
-        if (!httpVersion.equals("HTTP/1.1")) {
-          sendErrorResponse(out, 505);
-          return;
-        }
-
-        if (requestMethod.equals("GET")) {
-          handleGet(out, requestPath);
-        } else if (requestMethod.equals("POST")) {
-          handlePost(in, out, requestPath);
-        } else if (requestMethod.equals("PUT")) {
-          handlePut(in, out, requestPath);
-        } else if (requestMethod.equals("DELETE")) {
-          handleDelete(out, requestPath);
-        }
-
-        in.close();
-        out.close();
-        socket.close();
+          BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+  
+          String requestLine = in.readLine();
+          String[] requestLineParts = requestLine.split(" ");
+          String requestMethod = requestLineParts[0];
+          String requestPath = requestLineParts[1];
+          String httpVersion = requestLineParts[2];
+  
+          if (!httpVersion.equals("HTTP/1.1")) {
+              sendErrorResponse(out, 505);
+              return;
+          }
+  
+          if (requestMethod.equals("GET")) {
+              handleGet(out, requestPath);
+          } else if (requestMethod.equals("POST")) {
+              handlePost(in, out, requestPath);
+          } else if (requestMethod.equals("PUT")) {
+              handlePut(in, out, requestPath);
+          } else if (requestMethod.equals("DELETE")) {
+              handleDelete(out, requestPath);
+          } else if (requestMethod.equals("OPTIONS")) {
+              handleOptions(out, requestPath);
+          } else if (requestMethod.equals("HEAD")) {
+              handleHead(out, requestPath);
+          } else {
+              sendErrorResponse(out, 405);
+              return;
+          }
+  
+          in.close();
+          out.close();
+          socket.close();
       } catch (IOException e) {
-        e.printStackTrace();
+          e.printStackTrace();
+      }
+  }
+
+    private void handlePost(BufferedReader in, DataOutputStream out, String requestPath) throws IOException {
+      if (requestPath.endsWith(".txt")) {
+          StringBuilder requestBody = new StringBuilder();
+  
+          // Read the request body after processing request headers
+          String line;
+          while ((line = in.readLine()) != null && !line.isEmpty()) {
+              requestBody.append(line).append("\n");
+          }
+    
+      // Write the request body to the file
+      File file = new File(BASE_PATH + requestPath);
+      if (!file.exists()) {
+        file.createNewFile();
+      }
+      if (file.exists() && !file.isDirectory()) {
+        Files.write(file.toPath(), requestBody.toString().getBytes(), StandardOpenOption.APPEND);
+        out.writeBytes("HTTP/1.1 200 OK\r\n");
+        out.writeBytes("\r\n");
+      } else {
+        sendErrorResponse(out, 404);
+      }
+     }
+    }    
+
+    private void handlePut(BufferedReader in, DataOutputStream out, String requestPath) throws IOException {
+      if (requestPath.endsWith(".txt")) {
+        StringBuilder requestBody = new StringBuilder();
+    
+        // Read the request body after processing request headers
+        String line;
+        while ((line = in.readLine()) != null && !line.isEmpty()) {
+          requestBody.append(line).append("\n");
+        }
+    
+        // Write the request body to the file, creating a new file if it doesn't exist
+        File file = new File(BASE_PATH + requestPath);
+        if (!file.exists()) {
+          file.createNewFile();
+        }
+    
+        if (file.exists() && !file.isDirectory()) {
+          Files.write(file.toPath(), requestBody.toString().getBytes());
+          out.writeBytes("HTTP/1.1 200 OK\r\n");
+          out.writeBytes("\r\n");
+        } else {
+          sendErrorResponse(out, 404);
+        }
+      } else {
+        sendErrorResponse(out, 415);
       }
     }
 
@@ -103,57 +155,9 @@ public class MyHttpServer {
       }
     }
 
-    private void handlePost(BufferedReader in, DataOutputStream out, String requestPath) throws IOException {
-      if (requestPath.endsWith(".txt")) {
-        String requestBody = "";
-    
-        // read the request body
-        String line;
-        while ((line = in.readLine()) != null && !line.isEmpty()) {
-          requestBody += line + "\n";
-        }
-    
-        // append the request body to the file
-        File file = new File(BASE_PATH + requestPath);
-        if (file.exists() && !file.isDirectory()) {
-          Files.write(file.toPath(), requestBody.getBytes(), java.nio.file.StandardOpenOption.APPEND);
-          out.writeBytes("HTTP/1.1 200 OK\r\n");
-          out.writeBytes("\r\n");
-        } else {
-          sendErrorResponse(out, 404);
-        }
-      } else {
-        sendErrorResponse(out, 415);
-      }
-    }
-
-    private void handlePut(BufferedReader in, DataOutputStream out, String requestPath) throws IOException {
-      if (requestPath.endsWith(".txt")) {
-        String requestBody = "";
-        
-        // read the request body
-        String line;
-        while ((line = in.readLine()) != null && !line.isEmpty()) {
-          requestBody += line + "\n";
-        }
-        
-        // write the request body to the file
-        File file = new File(BASE_PATH + requestPath);
-        if (file.exists() && !file.isDirectory()) {
-          Files.write(file.toPath(), requestBody.getBytes());
-          out.writeBytes("HTTP/1.1 200 OK\r\n");
-          out.writeBytes("\r\n");
-        } else {
-          sendErrorResponse(out, 404);
-        }
-      } else {
-        sendErrorResponse(out, 415);
-      }
-    }
-    
     private void handleDelete(DataOutputStream out, String requestPath) throws IOException {
       File file = new File(BASE_PATH + requestPath);
-    
+
       if (file.exists() && !file.isDirectory()) {
         boolean deleted = file.delete();
         if (deleted) {
@@ -166,15 +170,43 @@ public class MyHttpServer {
         sendErrorResponse(out, 404);
       }
     }
-    
 
+    private void handleOptions(DataOutputStream out, String requestPath) throws IOException {
+      File file = new File(BASE_PATH + requestPath);
+  
+      String allowedMethods = "OPTIONS, GET, HEAD";
+      if (file.exists() && !file.isDirectory() && requestPath.endsWith(".txt")) {
+          allowedMethods += ", POST, PUT, DELETE";
+      }
+  
+      out.writeBytes("HTTP/1.1 200 OK\r\n");
+      out.writeBytes("Allow: " + allowedMethods + "\r\n");
+      out.writeBytes("\r\n");
+    }
+  
+    private void handleHead(DataOutputStream out, String requestPath) throws IOException {
+      File file = new File(BASE_PATH + requestPath);
+    
+      if (file.exists() && !file.isDirectory()) {
+        String[] parts = file.getName().split("\\.");
+        String extension = parts[parts.length - 1];
+        String mimeType = MIME_TYPES.getOrDefault(extension, "application/octet-stream");
+    
+        out.writeBytes("HTTP/1.1 200 OK\r\n");
+        out.writeBytes("Content-Type: " + mimeType + "\r\n");
+        out.writeBytes("\r\n");
+      } else {
+        sendErrorResponse(out, 404);
+      }
+    }
+    
     private void sendErrorResponse(DataOutputStream out, int errorCode) throws IOException {
       out.writeBytes("HTTP/1.1 " + errorCode + " " + getErrorMessage(errorCode) + "\r\n");
       out.writeBytes("Content-Type: text/plain\r\n");
       out.writeBytes("\r\n");
       out.writeBytes(getErrorMessage(errorCode));
     }
-    
+
     private String getErrorMessage(int errorCode) {
       switch (errorCode) {
         case 400:
@@ -183,6 +215,10 @@ public class MyHttpServer {
           return "Not Found";
         case 405:
           return "Method Not Allowed";
+        case 415:
+          return "Unsupported Media Type";
+        case 500:
+          return "Internal Server Error";
         case 505:
           return "HTTP Version Not Supported";
         default:
@@ -191,4 +227,4 @@ public class MyHttpServer {
     }
   }
 }
-    
+
